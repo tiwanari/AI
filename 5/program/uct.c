c93ced6eedda7a7c7e4f0e0a136e85885e501441
tiwanari <tatsuyaw0c@gmail.com>
2013-12-12 23:17:55 +0900

    refactored methods

--- 5/program/uct.c
+++ 5/program/uct.c
@@ -42,21 +42,52 @@ void free_nodes(node_t mother)
 }
 
 /**
+ * ゲームオーバーの手か
+ * @param int pos 手
+ * @param stone_t turn 現在の手番
+ * @param stone_t board[][] 盤面
+ * @return true:1, false:0
+ */
+int is_terminate_hand(int pos, stone_t turn, const stone_t board[HIGHT][AREA])
+{
+	stone_t copy[HIGHT][AREA];
+	int layer;
+	copy_board(board, copy);
+
+	layer = put_stone(pos, turn, copy);
+
+	return check_win(layer, pos, turn, copy) || is_full(copy);
+}
+
+/**
  * プレイアウトを行うシミュレーション関数
+ * @param int first 初めの手
  * @param stone_t mycolor 自分の色
  * @param stone_t turn 現在の手番
  * @param stone_t board[][] 盤面
  * @return win:1, other:0
  */
-int playout(stone_t mycolor, stone_t turn, stone_t board[HIGHT][AREA])
+int playout(int first, stone_t mycolor, stone_t turn, stone_t board[HIGHT][AREA])
 {
 	stone_t t = turn;
+	int layer = put_stone(first, t, board);
 
-	for (; ; t = REVERSE(t)) {
+	// 一手目での勝利判定
+	if (check_win(layer, first, t, board)) {
+		if (t == mycolor)
+			return 1;
+		else
+			return 0;
+	}
+	if (is_full(board))
+		return 0;
+
+	// 続きから
+	for (t = REVERSE(t); ; t = REVERSE(t)) {
 		int hands[AREA];
 		int nhands = get_legal_hands(board, hands);
 		int index = rand() % nhands;
-		int layer = put_stone(hands[index], t, board);
+		layer = put_stone(hands[index], t, board);
 		
 		if (check_win(layer, hands[index], t, board)) {
 			if (t == mycolor)
@@ -109,7 +140,6 @@ void expand_child(node_t mother, stone_t mycolor, stone_t turn, const stone_t bo
 	int hands[AREA];
 	int total_playout = 0;
 	int total_win = 0;
-	int layer;
 	int i;
 	
 	// 展開済みなら終了
@@ -125,20 +155,7 @@ void expand_child(node_t mother, stone_t mycolor, stone_t turn, const stone_t bo
 		mother->children[i]->moth = mother;
 		mother->children[i]->pos = hands[i];
 		total_playout += ++(mother->children[i]->playout);
-		
-		// 置く
-		layer = put_stone(hands[i], turn, copy);
-
-		// この時点で勝利か
-		if (check_win(layer, hands[i], turn, copy)) {
-			total_win += ++(mother->children[i]->win);
-		}
-		// 引き分け
-		else if(is_full(copy)) {
-			continue;
-		}
-
-		total_win += mother->children[i]->win = playout(mycolor, REVERSE(turn), copy);
+		total_win += mother->children[i]->win = playout(hands[i], mycolor, turn, copy);
 	}
 	propagate_result(mother, total_playout, total_win);	// 上位にも反映
 }
@@ -146,10 +163,9 @@ void expand_child(node_t mother, stone_t mycolor, stone_t turn, const stone_t bo
 /**
  * プレイアウトを行う子供を選択する
  * @param node_t mother 上位のノード
- * @param stone_t board[][] 現在の盤面
  * @return node_t プレイアウト行う子供
  */
-node_t select_child(node_t mother, const stone_t board[HIGHT][AREA])
+node_t select_child(node_t mother)
 {
 	double temp_ucb;
 	double max_ucb = -INIFINITY;
@@ -194,7 +210,7 @@ int uct(stone_t turn, const stone_t board[HIGHT][AREA])
 
 	// UCB値の計算と選択
 	while (root->playout < PLAYOUT_MAX) {
-		node_t candidate = select_child(root, board); // 最善手を選ぶ
+		node_t candidate = select_child(root); // 最善手を選ぶ
 		stone_t next[HIGHT][AREA];
 		int win;
 		int layer;
@@ -202,31 +218,21 @@ int uct(stone_t turn, const stone_t board[HIGHT][AREA])
 
 		// ボードをコピー
 		copy_board(board, next);
-		layer = put_stone(candidate->pos, cur_turn, next);
-		
-		// 勝利判定
-		if (check_win(layer, candidate->pos, cur_turn, next)) {
-			propagate_result(candidate, 1, 1);
-			continue;
-		}
-		// 終了判定
-		else if (is_full(next)) {
-			propagate_result(candidate, 1, 0);
-			continue;
-		}
+
 		// 終端でなく一定回数のプレイアウトを行っているなら展開
-		else if (candidate->playout >= EXPAND_THRESHOLD) {
+		if (!is_terminate_hand(candidate->pos, cur_turn, board) && candidate->playout >= EXPAND_THRESHOLD) {
+
+			// 手を進める
+			layer = put_stone(candidate->pos, cur_turn, next);
+
 			cur_turn = REVERSE(cur_turn);	// 手番を入れ替える
 
 			// 展開
-			expand_child(candidate, cur_turn, cur_turn, next);
-			candidate = select_child(candidate, next);	// その中で手を選ぶ
-
-			// 手を進める
-			put_stone(candidate->pos, cur_turn, next);
+			expand_child(candidate, turn, cur_turn, next);
+			candidate = select_child(candidate);	// その中で手を選ぶ
 		}
 		// プレイアウトを行う
-		win = playout(turn, REVERSE(cur_turn), next);
+		win = playout(candidate->pos, turn, cur_turn, next);
 		propagate_result(candidate, 1, win);	// 上位にも反映
 	}
 
